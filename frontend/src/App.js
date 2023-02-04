01436b8d31ded34f889896e0ba20366d7f36634f
robinhood1712 <ademolaadewoleo1@gmail.com>
2023-02-05 00:28:35 +0100

    created navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,37 @@
 import { useEffect } from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./scenes/global/Navbar";
 import Home from "./scenes/home/Home"
+// import Footer from "./scenes/global/Footer";
 import ItemDetails from "./scenes/itemDetails/ItemDetails";
+// import CartMenu from "./scenes/global/CartMenu";
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
-import Navbar from "./scenes/global/Navbar";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
-
-  const ScrollToTop = () => {
-    const { pathname } = useLocation()
-
-    useEffect(() => {
-      window.scrollTo(0, 0)
-    }, [pathname])
-
-    return null
-  }
-
-
   return (
     <div className="app">
       <BrowserRouter>
         <Navbar />
-        <ScrollToTop>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="item/item:id" element={<ItemDetails />} />
-            <Route path="checkout" element={<Checkout />} />
-            <Route path="checkout/success" element={<Confirmation />} />
-          </Routes>
-        </ScrollToTop>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/item/:itemId" element={<ItemDetails />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout/success" element={<Confirmation />} />
+        </Routes>
+        {/* <CartMenu />
+        <Footer /> */}
       </BrowserRouter>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,26 @@ import './index.css';
 import App from './App';
 import { ThemeProvider } from "@mui/material/styles"
 import { CssBaseline } from "@mui/material"
-import theme from "../src/theme"
+import { theme } from "../src/theme"
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import CartReducer from './redux/CartSlice';
+
+const store = configureStore({
+  reducer: { cart: CartReducer }
+})
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
